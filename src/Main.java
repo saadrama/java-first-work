381a2acd5aef5f5f7e61208eccc6092bf9fb0708
Ramah Saad <ramahsaad20@gmail.com>
2023-05-27 12:57:46 +0300

    my first java program

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] Args){
+System.out.println("I love java language");
+//mathematical calculations inside the println
+        System.out.println(3+3);
+        System.out.println(3*3);
+        //Declaring varriables
+        String name ="Saad";
+        int age=25;
+        age=23; //this is how you can assign a new value to an existing varriable
+        System.out.println(name +" of age "+age);
+        //Final variables
+        final int Age =24;
+        //How to declare other types of variables
+        int myNum=34;  //integer (whole number)
+        float myFloat=5.78f; //floating point number
+        char myGrade='A'; //character
+        boolean myBool=true;
+        String myText="hello";
+        //Java Identifiers
+            /*it is recommended to use descriptive names in order to create understandable and maintaintable code*/
+            int minutesPerHour =60;
+            //Java characters
+            char myGrde='A';
+            System.out.println( "My grade is " +myGrde);
+/*if you are fammiliar to ASCII values, you can use those to display csrtain caracters*/
+            char myChr=65, myChr1=66;
+            System.out.println(myChr + " " +myChr1);
+
+    }
+}
